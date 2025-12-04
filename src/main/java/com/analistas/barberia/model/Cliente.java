b1fab55d3c67a21dfbc33b56533c91d2dbbc3454
deverick1 <alexismarangoni05@gmail.com>
2025-12-04 16:33:24 -0300

    Configure Application Properties y cree la clase Cliente

---
+++ src/main/java/com/analistas/barberia/model/Cliente.java
@@ -0,0 +1,23 @@
+package com.analistas.barberia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_cliente;
+    private String nombre;
+    private String telefono;
+
+
+}
