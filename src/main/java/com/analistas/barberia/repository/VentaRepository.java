60da9f17c99d4233521bdbfcaf30c7907381bc40
deverick1 <alexismarangoni05@gmail.com>
2025-12-08 18:37:33 -0300

    Implementación de la interfaz VentaRepository para gestionar operaciones de la entidad Venta

---
+++ src/main/java/com/analistas/barberia/repository/VentaRepository.java
@@ -0,0 +1,9 @@
+package com.analistas.barberia.repository;
+
+import com.analistas.barberia.model.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Long>{
+    
+
+}
